d268b6e38ade37c6feddf914187e726d63a7c11d
ahmedsaoud31 <thefox70002000@gamil.com>
2013-06-18 15:58:49 +0300

    Update jquery.snippet.run.js

--- js/jquery.snippet.run.js
+++ js/jquery.snippet.run.js
@@ -6,24 +6,27 @@
 * 06/18/2013
 * Released under LGPL License.
 */
-$(function(){
-  var code;
-	$("code.snippet").each(function(){
-		goSnippet($(this));
+function runSnippet(){
+	$(function(){
+		var code;
+		$("code.snippet").each(function(){
+			goSnippet($(this));
+		});
+		function goSnippet(element){
+			code = $(element).html();
+			code = code.replace(/\<\!\-\-/g,"");
+			code = code.replace(/\-\-\>/g,"");
+			$(element).html(code);
+			code = $(element).find('pre').html();
+			code = code.replace(/\{HTMLCOMMENTSTART\}/g,"&LT;!--");
+			code = code.replace(/\{HTMLCOMMENTEND\}/g,"--&GT;");
+			code = code.replace(/\{HTMLLT\}/g,"&LT;");
+			code = code.replace(/\{HTMLGT\}/g,"&GT;");
+			var lang = $(element).find('pre').attr('lang');
+			var settings = JSON.parse($(element).find('pre').attr('settings'));
+			$(element).html('<pre>'+code+'</pre>');
+			$(element).find('pre').snippetcache('session', lang, settings);
+		}
 	});
-	function goSnippet(element){
-		code = $(element).html();
-		code = code.replace(/\<\!\-\-/g,"");
-		code = code.replace(/\-\-\>/g,"");
-		$(element).html(code);
-		code = $(element).find('pre').html();
-		code = code.replace(/\{HTMLCOMMENTSTART\}/g,"&LT;!--");
-		code = code.replace(/\{HTMLCOMMENTEND\}/g,"--&GT;");
-		code = code.replace(/\{HTMLLT\}/g,"&LT;");
-		code = code.replace(/\{HTMLGT\}/g,"&GT;");
-		var lang = $(element).find('pre').attr('lang');
-		var settings = JSON.parse($(element).find('pre').attr('settings'));
-		$(element).html('<pre>'+code+'</pre>');
-		$(element).find('pre').snippetcache('session', lang, settings);
-	}
-});
+}
+runSnippet();
