fb03caeb1a648db4d190a398c0857812864c7ef2
Ahmed Aboelsaoud Ahmed <thefox70002000@gmail.com>
2013-07-04 05:27:15 +0200

    Update jquery.snippet.run.js

--- js/jquery.snippet.run.js
+++ js/jquery.snippet.run.js
@@ -1,31 +1,26 @@
 /*
 * snippet plugin
-* v1.0
+* v1.1
 * Written by Ahmed Aboelsaoud
 * https://github.com/ahmedsaoud31/snippet
-* 06/18/2013
+* Date 06/17/2013
+* Update 07/04/2013
 * Released under LGPL License.
 */
 function runSnippet(){
 	$(function(){
-		var code;
-		$("code.snippet").each(function(){
+		var code,lang,settings;
+		$("div.snippet").each(function(){
 			goSnippet($(this));
 		});
 		function goSnippet(element){
-			code = $(element).html();
-			code = code.replace(/\<\!\-\-/g,"");
-			code = code.replace(/\-\-\>/g,"");
-			$(element).html(code);
-			code = $(element).find('pre').html();
-			code = code.replace(/\{HTMLCOMMENTSTART\}/g,"&LT;!--");
-			code = code.replace(/\{HTMLCOMMENTEND\}/g,"--&GT;");
-			code = code.replace(/\{HTMLLT\}/g,"&LT;");
-			code = code.replace(/\{HTMLGT\}/g,"&GT;");
-			var lang = $(element).find('pre').attr('lang');
-			var settings = JSON.parse($(element).find('pre').attr('settings'));
-			$(element).html('<pre>'+code+'</pre>');
-			$(element).find('pre').snippetcache('session', lang, settings);
+			$(element).removeAttr('style');
+			$(element).removeAttr('contenteditable');
+			code = $(element).find('.code').html().replace(/\<\!\-\-\{/g,"").replace(/\}\-\-\>/g,"");
+			lang = $(element).find('.lang').html().replace(/\<\!\-\-\{/g,"").replace(/\}\-\-\>/g,"");
+			settings = JSON.parse($(element).find('.settings').html().replace(/\<\!\-\-\{/g,"").replace(/\}\-\-\>/g,""));
+			$(element).find('pre').html(code);
+			$(element).find('pre').snippet(lang, settings);
 		}
 	});
 }
