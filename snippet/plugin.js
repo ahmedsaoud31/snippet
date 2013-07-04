62f6ceca5cf657a11088cc363fa76e3dd38a2b14
Ahmed Aboelsaoud Ahmed <thefox70002000@gmail.com>
2013-07-04 05:27:45 +0200

    Update plugin.js

--- snippet/plugin.js
+++ snippet/plugin.js
@@ -1,19 +1,161 @@
 /*
 * snippet plugin
-* v1.0
-* By Ahmed Aboelsaoud
+* v1.1
+* Written by Ahmed Aboelsaoud
+* https://github.com/ahmedsaoud31/snippet
 * Date 06/17/2013
-* https://gethub.com/ahmedsaoud31
+* Update 07/04/2013
+* Released under LGPL License.
 */
 tinymce.PluginManager.add('snippet', function(editor) {
 	var code = '';
 	var defaultStyle = 'acid';
 	var settings = {};
-	settings['style'] = defaultStyle;
-	var lang = defaultLang = 'html';
+	var defaultLang = 'html';
+	var lang;
 	var data = {};
 	function showDialog() {
-		data.codebox = editor.selection.getContent({format : 'text'});
+		var selection = editor.selection, dom = editor.dom, thisSelectCode,selctedCode,selectFlag=false;
+		settings = {};
+		lang = '';
+		settings['style'] = '';
+		data.collapse = false;
+		data.menu = true;
+		data.showNum = true;
+		data.transparent = false;
+		var langArr = [	{text:'Shoose Language',value:'0'},
+						{text:'C',value:'c'},
+						{text:'C#',value:'csharp'},
+						{text:'C++',value:'cpp'},
+						{text:'CSS',value:'css'},
+						{text:'Flex',value:'flex'},
+						{text:'HTML',value:'html'},
+						{text:'Java',value:'java'},
+						{text:'JavaScript',value:'javascript'},
+						{text:'JavaScript with DOM',value:'javascript_dom'},
+						{text:'Perl',value:'perl'},
+						{text:'PHP',value:'php'},
+						{text:'Python',value:'python'},
+						{text:'Ruby',value:'ruby'},
+						{text:'SQL',value:'sql'},
+						{text:'XML',value:'xml'}
+					];
+		var styleArr = [{text:'Select Style',value:'0'},
+						{text:'acid',value:'acid'},
+						{text:'berries-dark',value:'berries-dark'},
+						{text:'berries-light',value:'berries-light'},
+						{text:'bipolar',value:'bipolar'},
+						{text:'blacknblue',value:'blacknblue'},
+						{text:'contrast',value:'contrast'},
+						{text:'darkblue',value:'darkblue'},
+						{text:'darkness',value:'darkness'},
+						{text:'desert',value:'desert'},
+						{text:'dull',value:'dull'},
+						{text:'easter',value:'easter'},
+						{text:'emacs',value:'emacs'},
+						{text:'golden',value:'golden'},
+						{text:'greenlcd',value:'greenlcd'},
+						{text:'ide-anjuta',value:'ide-anjuta'},
+						{text:'ide-codewarrior',value:'ide-codewarrior'},
+						{text:'ide-devcpp',value:'ide-devcpp'},
+						{text:'ide-eclipse',value:'ide-eclipse'},
+						{text:'ide-kdev',value:'ide-kdev'},
+						{text:'ide-msvcpp',value:'ide-msvcpp'},
+						{text:'kwrite',value:'kwrite'},
+						{text:'matlab',value:'matlab'},
+						{text:'navy',value:'navy'},
+						{text:'nedit',value:'nedit'},
+						{text:'neon',value:'neon'},
+						{text:'night',value:'night'},
+						{text:'pablo',value:'pablo'},
+						{text:'peachpuff',value:'peachpuff'},
+						{text:'rand01',value:'rand01'},
+						{text:'random',value:'random'},
+						{text:'the',value:'the'},
+						{text:'typical',value:'typical'},
+						{text:'vampire',value:'vampire'},
+						{text:'vim',value:'vim'},
+						{text:'vim-dark',value: 'vim-dark'},
+						{text:'whatis',value:'whatis'},
+						{text:'whitengrey',value:'whitengrey'},
+						{text:'zellner',value:'zellner'}
+						];
+		thisSelectCode = selection.getNode();
+		
+		if(thisSelectCode.nodeName == 'DIV' && dom.getAttrib(thisSelectCode, 'class') == 'snippet'){
+			selectFlag=true;
+			lang = $('#textareaContent_ifr').contents().find(thisSelectCode).find('.lang').html();
+			lang = lang.replace(/\<\!\-\-\{/g,"").replace(/\}\-\-\>/g,"");
+			settings = $('#textareaContent_ifr').contents().find(thisSelectCode).find('.settings').html();
+			settings = settings.replace(/\<\!\-\-\{/g,"").replace(/\}\-\-\>/g,"");
+			settings = JSON.parse(settings);
+			selctedCode = $('#textareaContent_ifr').contents().find(thisSelectCode).find('.code').html();
+			selctedCode = selctedCode.replace(/\<\!\-\-\{/g,"").replace(/\}\-\-\>/g,"").replace(/\&LT\;/g,"<").replace(/\&GT\;/g,">");
+		}else if((thisSelectCode.nodeName=='PRE' && dom.getAttrib(thisSelectCode,'class')=='snippet')||
+				(thisSelectCode.nodeName=='SPAN' && dom.getAttrib(thisSelectCode,'class')=='lang')||
+				(thisSelectCode.nodeName=='SPAN' && dom.getAttrib(thisSelectCode,'class')=='settings')||
+				(thisSelectCode.nodeName=='SPAN' && dom.getAttrib(thisSelectCode,'class')=='code')){
+			selectFlag=true;
+			thisSelectCode = dom.getParent(thisSelectCode, 'div.snippet');
+			lang = $('#textareaContent_ifr').contents().find(thisSelectCode).find('.lang').html();
+			lang = lang.replace(/\<\!\-\-\{/g,"").replace(/\}\-\-\>/g,"");
+			settings = $('#textareaContent_ifr').contents().find(thisSelectCode).find('.settings').html();
+			settings = settings.replace(/\<\!\-\-\{/g,"").replace(/\}\-\-\>/g,"");
+			settings = JSON.parse(settings);
+			selctedCode = $('#textareaContent_ifr').contents().find(thisSelectCode).find('.code').html();
+			selctedCode = selctedCode.replace(/\<\!\-\-\{/g,"").replace(/\}\-\-\>/g,"").replace(/\&LT\;/g,"<").replace(/\&GT\;/g,">");
+		}else{
+			selctedCode = selection.getContent({format : 'text'});
+		}
+		for(var i=0;i<langArr.length;i++){
+			if(langArr[i].value == lang){
+				langArr[i].selected = true;
+			}
+		}
+		for(var i=0;i<styleArr.length;i++){
+			if(styleArr[i].value == settings['style']){
+				styleArr[i].selected = true;
+			}
+		}
+		if(typeof settings.collapse != 'undefined') {
+			if(settings.collapse){
+				data.collapse = true;
+			}else{
+				data.collapse = false;
+			}
+		}
+		if(typeof settings.menu != 'undefined') {
+			if(settings.menu){
+				data.menu = true;
+			}else{
+				data.menu = false;
+			}
+		}
+		if(typeof settings.showNum != 'undefined') {
+			if(settings.showNum){
+				data.showNum = true;
+			}else{
+				data.showNum = false;
+			}
+		}
+		if(typeof settings.transparent != 'undefined') {
+			if(settings.transparent){
+				data.transparent = true;
+			}else{
+				data.transparent = false;
+			}
+		}
+		if(typeof settings.box != 'undefined') {
+			data.linesbox = settings.box;
+		}
+		if(lang == ''){
+			lang = defaultLang;
+		}
+		if(settings['style'] == ''){
+			settings['style'] = defaultStyle;
+		}
+		
+		data.codebox = selctedCode;
 		editor.windowManager.open({
 			title: 'Snippet',
 			data: data,
@@ -21,23 +163,7 @@ tinymce.PluginManager.add('snippet', function(editor) {
 					{
 						name: 'Language',
 						type: 'listbox',
-						values: [	{text: 'Shoose Language', value: '0'},
-									{text: 'C', value: 'c'},
-									{text: 'C#', value: 'csharp'},
-									{text: 'C++', value: 'cpp'},
-									{text: 'CSS', value: 'css'},
-									{text: 'Flex', value: 'flex'},
-									{text: 'HTML', value: 'html'},
-									{text: 'Java', value: 'java'},
-									{text: 'JavaScript', value: 'javascript'},
-									{text: 'JavaScript with DOM', value: 'javascript_dom'},
-									{text: 'Perl', value: 'perl'},
-									{text: 'PHP', value: 'php'},
-									{text: 'Python', value: 'python'},
-									{text: 'Ruby', value: 'ruby'},
-									{text: 'SQL', value: 'sql'},
-									{text: 'XML', value: 'xml'}
-									],
+						values: langArr,
 						onselect : function(v) {
 							if(this.value() == '0'){
 								lang = defaultLang;
@@ -47,48 +173,9 @@ tinymce.PluginManager.add('snippet', function(editor) {
 						 }
 					},
 					{
-						name: 'Style',
-						type: 'listbox',
-						values: [	{text: 'Select Style', value: '0'},
-									{text: 'acid', value: 'acid'},
-									{text: 'berries-dark', value: 'berries-dark'},
-									{text: 'berries-light', value: 'berries-light'},
-									{text: 'bipolar', value: 'bipolar'},
-									{text: 'blacknblue', value: 'blacknblue'},
-									{text: 'contrast', value: 'contrast'},
-									{text: 'darkblue', value: 'darkblue'},
-									{text: 'darkness', value: 'darkness'},
-									{text: 'desert', value: 'desert'},
-									{text: 'dull', value: 'dull'},
-									{text: 'easter', value: 'easter'},
-									{text: 'emacs', value: 'emacs'},
-									{text: 'golden', value: 'golden'},
-									{text: 'greenlcd', value: 'greenlcd'},
-									{text: 'ide-anjuta', value: 'ide-anjuta'},
-									{text: 'ide-codewarrior', value: 'ide-codewarrior'},
-									{text: 'ide-devcpp', value: 'ide-devcpp'},
-									{text: 'ide-eclipse', value: 'ide-eclipse'},
-									{text: 'ide-kdev', value: 'ide-kdev'},
-									{text: 'ide-msvcpp', value: 'ide-msvcpp'},
-									{text: 'kwrite', value: 'kwrite'},
-									{text: 'matlab', value: 'matlab'},
-									{text: 'navy', value: 'navy'},
-									{text: 'nedit', value: 'nedit'},
-									{text: 'neon', value: 'neon'},
-									{text: 'night', value: 'night'},
-									{text: 'pablo', value: 'pablo'},
-									{text: 'peachpuff', value: 'peachpuff'},
-									{text: 'rand01', value: 'rand01'},
-									{text: 'random', value: 'random'},
-									{text: 'the', value: 'the'},
-									{text: 'typical', value: 'typical'},
-									{text: 'vampire', value: 'vampire'},
-									{text: 'vim', value: 'vim'},
-									{text: 'vim-dark', value: 'vim-dark'},
-									{text: 'whatis', value: 'whatis'},
-									{text: 'whitengrey', value: 'whitengrey'},
-									{text: 'zellner', value: 'zellner'}
-								],
+						name:'Style',
+						type:'listbox',
+						values:styleArr,
 						onselect : function(v) {
 							if(this.value() == '0'){
 								settings['style'] = defaultStyle;
@@ -106,6 +193,7 @@ tinymce.PluginManager.add('snippet', function(editor) {
 						items: [
 							{
 								type: 'checkbox',
+								checked:data.collapse,
 								text: 'hide/show Code',
 								onclick: function(){
 									settings['collapse'] = this.value();
@@ -113,7 +201,7 @@ tinymce.PluginManager.add('snippet', function(editor) {
 							},
 							{
 								type: 'checkbox',
-								checked: true,
+								checked:data.menu,
 								text: 'show Menu',
 								onclick: function(){
 									settings['menu'] = this.value();
@@ -121,7 +209,7 @@ tinymce.PluginManager.add('snippet', function(editor) {
 							},
 							{
 								type: 'checkbox',
-								checked: true,
+								checked:data.showNum,
 								text: 'show Numbers',
 								onclick: function(){
 									settings['showNum'] = this.value();
@@ -129,6 +217,7 @@ tinymce.PluginManager.add('snippet', function(editor) {
 							},
 							{
 								type: 'checkbox',
+								checked:data.transparent,
 								text: 'Transparent',
 								onclick: function(){
 									settings['transparent'] = this.value();
@@ -180,18 +269,24 @@ tinymce.PluginManager.add('snippet', function(editor) {
 					}
 			],
 			onsubmit: function(e) {
-				code = e.data.codebox;
-				code = code.replace(/\<\!\-\-/g,"{HTMLCOMMENTSTART}");
-				code = code.replace(/\-\-\>/g,"{HTMLCOMMENTEND}");
-				code = code.replace(/\</g,"{HTMLLT}");
-				code = code.replace(/\>/g,"{HTMLGT}");
-				var lines = e.data.linesbox.replace(/[^1-9,-]/g,"");
+				var code2 = code = e.data.codebox;
+				code2 = code2.replace(/\</g,"&LT;");
+				code2 = code2.replace(/\>/g,"&GT;");
+				code2 = code2.replace(/\n/g,"<br>");
+				code2 = code2.replace(/\t/g,"&nbsp;&nbsp;&nbsp;&nbsp;");
+				code2 = code2.replace(/\s/g,"&nbsp;");
+				code = code.replace(/\</g,"&LT;");
+				code = code.replace(/\>/g,"&GT;");
+				var lines = e.data.linesbox.replace(/[^0-9,-]/g,"");
 				if(lines != ''){
 					settings['box'] = lines;
 				}
-				editor.insertContent('<code class="snippet" style="background-color: #EEE;">{'+lang+' Code}<!--<pre lang="'+lang+'" settings=\''+JSON.stringify(settings)+'\'>'+code+'</pre>--></code>',{format: 'text'});
+				if(selectFlag){
+					editor.dom.remove(thisSelectCode);
+				}
+				editor.insertContent('<br><div contenteditable="false" dir="ltr" class="snippet" style="background-color: #DDD; margin: 5px 0 5px 0;"><pre class="snippet">'+code2+'</pre><span class="lang"><!--{'+lang+'}--></span><span class="settings"><!--{'+JSON.stringify(settings)+'}--></span><span class="code"><!--{'+code+'}--></span></div><br>',{format: 'text'});
 				settings = {};
-				lang=defaultLang;
+				lang = defaultLang;
 			}
 		});
 	}
